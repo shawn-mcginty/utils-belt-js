4231152d8402e8ac8944721b3e8ae1b54bf1e7e2
Ginny Katherine McGinty <shawn@codouken.com>
2015-04-02 07:52:31 -0500

    Fixed an issue with arrayutils

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var StringUtils = require('./utils/stringUtils');
+var ArrayUtils = require('./utils/arrayUtils');
 
 var UtilsBelt = {};
 
 UtilsBelt.StringUtils = StringUtils;
+UtilsBelt.ArrayUtils = ArrayUtils;
 
 module.exports = UtilsBelt;
